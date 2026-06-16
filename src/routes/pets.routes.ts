22e24fe6008cccfefdbd723534b29020ad836e16
Ekaterina Varentsova <kotya.v.gr@gmail.com>
2026-06-16 15:48:17 +0300

    feat: сделано редактирование pet

--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -9,11 +9,10 @@ router.get('/dogs', validateTokenMiddleware, PetsController.getPetsDogs);
 router.get('/cats', validateTokenMiddleware, PetsController.getPetsCats);
 router.get('/:id', PetsController.getPetId);
 router.post('/dogs', validateTokenMiddleware, PetsController.createPet, PetsController.getPetsDogs);
-router.post('/cats', validateTokenMiddleware, PetsController.createPet, PetsController.getPetsDogs);
+router.post('/cats', validateTokenMiddleware, PetsController.createPet, PetsController.getPetsCats);
+router.put('/dogs/:id', validateTokenMiddleware, PetsController.updatePet, PetsController.getPetsDogs);
+router.put('/cats/:id', validateTokenMiddleware, PetsController.updatePet, PetsController.getPetsCats);
 router.delete('/dogs/:id', validateTokenMiddleware, PetsController.deletePet, PetsController.getPetsDogs);
 router.delete('/cats/:id', validateTokenMiddleware, PetsController.deletePet, PetsController.getPetsCats);
-// router.put('/:id', PetsController.updatePet);
-
-
 
 export default router;
